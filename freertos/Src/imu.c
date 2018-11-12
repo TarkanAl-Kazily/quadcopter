582a59d2f22264c2b5f19d6b928d3dc2ce30e629
Tarkan Al-Kazily <tarkucar@uw.edu>
2018-11-12 10:27:34 -0800

    In progress commit

--- freertos/Src/imu.c
+++ freertos/Src/imu.c
@@ -17,7 +17,7 @@ uint16_t imu_accel_z;
 
 // PRIVATE VARIABLES
 
-#define IMU_ADDRESS           0x68
+#define IMU_ADDRESS           0x69
 I2C_HandleTypeDef *imu_i2c_h;
 
 #define REG_READ              0x00
@@ -49,13 +49,14 @@ I2C_HandleTypeDef *imu_i2c_h;
 #define IMU_REG_GYRO_ZOUT_H     71
 #define IMU_REG_GYRO_ZOUT_L     72
 
+#define IMU_REG_WHO_AM_I       117
+
 // HELPER FUNCTIONS
 
 // Writes len bytes to addr, starting at reg. Returns 0 on success.
 static int reg_write(uint16_t addr, uint8_t reg, uint8_t *data, uint16_t len) {
   reg |= REG_WRITE;
-  HAL_StatusTypeDef ret = HAL_I2C_Master_Transmit(imu_i2c_h, addr, &reg, 1, 1000);
-  ret = HAL_I2C_Master_Transmit(imu_i2c_h, addr, data, len, 1000);
+  HAL_I2C_Mem_Write(imu_i2c_h, addr, reg, I2C_MEMADD_SIZE_8BIT, data, len, 1000);
 
   return 0;
 }
@@ -63,8 +64,7 @@ static int reg_write(uint16_t addr, uint8_t reg, uint8_t *data, uint16_t len) {
 // Reads len bytes from addr, starting at reg. Returns 0 on success.
 static int reg_read(uint16_t addr, uint8_t reg, uint8_t *data, uint16_t len) {
   reg |= REG_READ;
-  HAL_StatusTypeDef ret = HAL_I2C_Master_Transmit(imu_i2c_h, addr, &reg, 1, 1000);
-  ret = HAL_I2C_Master_Receive(imu_i2c_h, addr, data, len, 1000);
+  HAL_I2C_Mem_Read(imu_i2c_h, addr, reg, I2C_MEMADD_SIZE_8BIT, data, len, 1000);
 
   return 0;
 }
@@ -73,6 +73,17 @@ static int reg_read(uint16_t addr, uint8_t reg, uint8_t *data, uint16_t len) {
 
 int imu_init(I2C_HandleTypeDef *i2c_h) {
   imu_i2c_h = i2c_h;
+  uint8_t data = IMU_REG_WHO_AM_I;
+
+  /* Send address */
+  int res = HAL_I2C_Master_Transmit(imu_i2c_h, IMU_ADDRESS, &data, 1, 1000);
+
+  /* Receive multiple byte */
+  res = HAL_I2C_Master_Receive(imu_i2c_h, IMU_ADDRESS, &data, 1, 1000);
+
+  while (data != IMU_ADDRESS) {
+    ////// ARG
+  }
 
   // Start a calibration
 
@@ -89,7 +100,8 @@ int imu_init(I2C_HandleTypeDef *i2c_h) {
     imu_accel_x = (buf[0] << 8) | buf[1];
     imu_accel_y = (buf[2] << 8) | buf[3];
     imu_accel_z = (buf[4] << 8) | buf[5];
-    HAL_Delay(500);
+
+    for (uint16_t i = 0; i < 500000; i++);
   }
   return 0;
 }
