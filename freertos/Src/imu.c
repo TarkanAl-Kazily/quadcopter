39bbc548a56e249a916aa615b4c115a0814dd15a
Tarkan Al-Kazily <tarkucar@uw.edu>
2018-11-13 21:13:03 -0800

    Reads sensor data, but does not run actual code. Must use GDB to view

--- freertos/Src/imu.c
+++ freertos/Src/imu.c
@@ -11,61 +11,101 @@
 
 // PUBLIC VARIABLES
 
-uint16_t imu_accel_x;
-uint16_t imu_accel_y;
-uint16_t imu_accel_z;
+int16_t imu_accel_x;
+int16_t imu_accel_y;
+int16_t imu_accel_z;
 
 // PRIVATE VARIABLES
 
-#define IMU_ADDRESS           0x69
+// Must shift address left one bit for HAL APIs
+#define IMU_ADDRESS           (0x68 << 1)
 I2C_HandleTypeDef *imu_i2c_h;
 
-#define REG_READ              0x00
-#define REG_WRITE             0x80
-
 // Registers
-#define IMU_REG_CONFIG          26
-#define IMU_REG_GYRO_CONFIG     27
-#define IMU_REG_ACCEL_CONFIG    28
+#define IMU_REG_CONFIG               26
+#define IMU_REG_GYRO_CONFIG          27
+#define IMU_REG_ACCEL_CONFIG         28
+#define IMU_REG_ACCEL_CONFIG_2       29
 
-#define IMU_REG_INT_PIN_CFG     55
-#define IMU_REG_INT_ENABLE      56
-#define IMU_REG_INT_STATUS      57
+#define IMU_REG_INT_PIN_CFG          55
+#define IMU_REG_INT_ENABLE           56
+#define IMU_REG_INT_STATUS           57
 
-#define IMU_REG_ACCEL_XOUT_H    59
-#define IMU_REG_ACCEL_XOUT_L    60
-#define IMU_REG_ACCEL_YOUT_H    61
-#define IMU_REG_ACCEL_YOUT_L    62
-#define IMU_REG_ACCEL_ZOUT_H    63
-#define IMU_REG_ACCEL_ZOUT_L    64
+#define IMU_REG_ACCEL_XOUT_H         59
+#define IMU_REG_ACCEL_XOUT_L         60
+#define IMU_REG_ACCEL_YOUT_H         61
+#define IMU_REG_ACCEL_YOUT_L         62
+#define IMU_REG_ACCEL_ZOUT_H         63
+#define IMU_REG_ACCEL_ZOUT_L         64
 
-#define IMU_REG_TEMP_OUT_H      65
-#define IMU_REG_TEMP_OUT_L      66
+#define IMU_REG_TEMP_OUT_H           65
+#define IMU_REG_TEMP_OUT_L           66
 
-#define IMU_REG_GYRO_XOUT_H     67
-#define IMU_REG_GYRO_XOUT_L     68
-#define IMU_REG_GYRO_YOUT_H     69
-#define IMU_REG_GYRO_YOUT_L     70
-#define IMU_REG_GYRO_ZOUT_H     71
-#define IMU_REG_GYRO_ZOUT_L     72
+#define IMU_REG_GYRO_XOUT_H          67
+#define IMU_REG_GYRO_XOUT_L          68
+#define IMU_REG_GYRO_YOUT_H          69
+#define IMU_REG_GYRO_YOUT_L          70
+#define IMU_REG_GYRO_ZOUT_H          71
+#define IMU_REG_GYRO_ZOUT_L          72
 
-#define IMU_REG_WHO_AM_I       117
+#define IMU_REG_SIGNAL_PATH_RESET   104
+
+#define IMU_REG_USER_CTRL           106
+#define IMU_REG_PWR_MGMT_1          107
+#define IMU_REG_PWR_MGMT_2          108
+
+#define IMU_REG_WHO_AM_I            117
 
 // HELPER FUNCTIONS
 
 // Writes len bytes to addr, starting at reg. Returns 0 on success.
 static int reg_write(uint16_t addr, uint8_t reg, uint8_t *data, uint16_t len) {
-  reg |= REG_WRITE;
-  HAL_I2C_Mem_Write(imu_i2c_h, addr, reg, I2C_MEMADD_SIZE_8BIT, data, len, 1000);
-
-  return 0;
+  return HAL_I2C_Mem_Write(imu_i2c_h, addr, reg, I2C_MEMADD_SIZE_8BIT, data, len, 1000);
 }
 
 // Reads len bytes from addr, starting at reg. Returns 0 on success.
 static int reg_read(uint16_t addr, uint8_t reg, uint8_t *data, uint16_t len) {
-  reg |= REG_READ;
-  HAL_I2C_Mem_Read(imu_i2c_h, addr, reg, I2C_MEMADD_SIZE_8BIT, data, len, 1000);
+  return HAL_I2C_Mem_Read(imu_i2c_h, addr, reg, I2C_MEMADD_SIZE_8BIT, data, len, 1000);
+}
 
+// Initializes the main MPU
+static uint8_t imu_device_init() {
+  uint8_t data;
+  // Reset the internal registers
+  data = 0x80;
+  reg_write(IMU_ADDRESS, IMU_REG_PWR_MGMT_1, &data, 1);
+  HAL_Delay(100);
+  // Reset gyro/accel/temp digital signal path
+  data = 0x07;
+  reg_write(IMU_ADDRESS, IMU_REG_SIGNAL_PATH_RESET, &data, 1);
+  HAL_Delay(100);
+
+  uint8_t IMU_REG_Init_Data[7][2] = {
+    { IMU_REG_PWR_MGMT_1,     0x03 }, // Sets Clock Source to the Z axis gyro
+    { IMU_REG_PWR_MGMT_2,     0x00 }, // all enable
+    { IMU_REG_CONFIG,         0x04 }, // set frequency of samples
+    { IMU_REG_GYRO_CONFIG,    0x18 }, // +-2000dps
+    { IMU_REG_ACCEL_CONFIG,   0x10 }, // +-8G
+    { IMU_REG_ACCEL_CONFIG_2, 0x04 }, // acc bandwidth 20Hz
+    { IMU_REG_USER_CTRL,      0x20 }, // Enable the I2C Master I/F module
+    // pins ES_DA and ES_SCL are isolated from 
+    // pins SDA/SDI and SCL/SCLK.
+  };
+  uint8_t i = 0;
+  for (i = 0; i < 7; i++)
+  {
+    reg_write(IMU_ADDRESS, IMU_REG_Init_Data[i][0], &IMU_REG_Init_Data[i][1], 1);
+    HAL_Delay(1);
+  }
+
+  /*
+  // Reset ist
+  HAL_GPIO_WritePin(IST_SET_GPIO_Port, IST_SET_Pin, GPIO_PIN_RESET);
+  HAL_Delay(2000);
+  HAL_GPIO_WritePin(IST_SET_GPIO_Port, IST_SET_Pin, GPIO_PIN_SET);
+  HAL_Delay(2000);
+  */
+  //mpu_offset_cal();
   return 0;
 }
 
@@ -73,26 +113,16 @@ static int reg_read(uint16_t addr, uint8_t reg, uint8_t *data, uint16_t len) {
 
 int imu_init(I2C_HandleTypeDef *i2c_h) {
   imu_i2c_h = i2c_h;
-  uint8_t data = IMU_REG_WHO_AM_I;
+  uint8_t buf[6];
+  uint8_t addr;
 
   /* Send address */
-  int res = HAL_I2C_Master_Transmit(imu_i2c_h, IMU_ADDRESS, &data, 1, 1000);
-
-  /* Receive multiple byte */
-  res = HAL_I2C_Master_Receive(imu_i2c_h, IMU_ADDRESS, &data, 1, 1000);
-
-  while (data != IMU_ADDRESS) {
-    ////// ARG
-  }
+  reg_read(IMU_ADDRESS, IMU_REG_WHO_AM_I, &addr, 1);
 
   // Start a calibration
+  imu_device_init();
 
   // Start a calculation
-
-  uint8_t buf[6];
-  buf[0] = 0x00;
-  // Set ranges to be 0
-  reg_write(IMU_ADDRESS, IMU_REG_ACCEL_CONFIG, buf, 1);
 
   while (1) {
     // Read back x, y, z accelerometer data
@@ -101,7 +131,7 @@ int imu_init(I2C_HandleTypeDef *i2c_h) {
     imu_accel_y = (buf[2] << 8) | buf[3];
     imu_accel_z = (buf[4] << 8) | buf[5];
 
-    for (uint16_t i = 0; i < 500000; i++);
+    HAL_Delay(5);
   }
   return 0;
 }
